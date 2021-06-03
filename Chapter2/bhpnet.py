f3cc2a5d4df44ac5b6b6dce86676c88e4c8438e3
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2021-06-03 19:23:38 +0100

    Chapter 2

--- Chapter2/bhpnet.py
+++ Chapter2/bhpnet.py
@@ -38,6 +38,50 @@ def usage():
     sys.exit(0)
 
 
+def client_sender(buffer):
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # establishing a connection w/ defined target host
+        client.connect((target, port))
+    
+        if len(buffer):
+            client.send(buffer.encode('utf-8'))
+
+        while True:
+
+            # wait for data back
+            recv_len = 1
+            response = b""
+
+            while recv_len:
+
+                data     = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+
+            print(response.decode('utf-8'), end=' ')
+
+            # wait for more input
+            buffer = input("")
+            buffer += "\n"
+
+            # send it off
+            client.send(buffer.encode('utf-8'))
+
+    except socket.error as exc:   
+    
+        # catching error and printing it
+        print("[*] Exception! Exiting.")
+        print(f"[*] Caught exception socket.error: {exc}")
+
+        # close connection
+        client.close()
+
 def main():
 
     global listen
@@ -51,6 +95,37 @@ def main():
     if not len(sys.argv[1:]):
        usage()
     
-    # red the commandline options
+    # read the commandline options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:"
+        opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:cu:", ["help", "listen", "execute", "target", "port", "command", "upload"])
+    except: getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--command"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+
+
+    if not listen and len(target) and port > 0:
+
+        buffer = sys.stdin.read()
+        client_sender(buffer)
+
+    if listen:
+        server_loop()
